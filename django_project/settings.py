383e0bfb3fdd0cf5ef9f75162b22604165981ca2
Tharcisse Irasubiza <38328472-irasubizatharc1@users.noreply.replit.com>
2024-12-18 11:42:27 +0000

    first commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -32,6 +32,7 @@ CSRF_TRUSTED_ORIGINS = ['https://09abf8de-7e8a-4ca4-9974-f249891d02c8-00-1r0tskj
 # Application definition
 
 INSTALLED_APPS = [
+    'account',
     'suggestionBox',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -121,7 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- suggestionBox/urls.py
+++ suggestionBox/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 
 from . import views
 urlpatterns=[
-  path('',views.suggestion_view,name='suggestion'),
+  path('suggestion/',views.suggestion_view,name='suggestion'),
   path('success/',views.success_view,name='success'),
 ]
